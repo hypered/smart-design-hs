e689e8492c1d385587550cd6decc3fde83170190
Vo Minh Thu <thu@hypered.io>
2022-07-27 15:46:53 +0200

    Add a generic error page.

--- lib/src/Smart/Html/Errors.hs
+++ lib/src/Smart/Html/Errors.hs
@@ -2,6 +2,7 @@
 
 module Smart.Html.Errors
   ( NotFound(..)
+  , Error(..)
   ) where
 
 import           Smart.Html.Shared.Html.Icons
@@ -9,15 +10,27 @@ import Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
 
+
+--------------------------------------------------------------------------------
 data NotFound = NotFound
 
 instance H.ToMarkup NotFound where
   -- TODO Move the `main` element elsewhere.
   toMarkup NotFound =
+    H.toMarkup (Error 404 "Page not found"
+      "Sorry, we couldn't find the page you are looking for.")
+
+
+--------------------------------------------------------------------------------
+data Error = Error Int Text Text -- code, title, subtitle
+
+instance H.ToMarkup Error where
+  -- TODO Move the `main` element elsewhere.
+  toMarkup (Error code title subtitle) =
       H.div ! A.class_ "c-error-page" $ do
-        H.span ! A.class_ "c-error-page__code" $ "404"
-        H.h4 ! A.class_ "c-error-page__title" $ "Page not found"
-        H.p ! A.class_ "c-error-page__desc" $ "Sorry, we couldn't find the page you are looking for."
+        H.span ! A.class_ "c-error-page__code" $ H.toHtml $ (show code :: Text)
+        H.h4 ! A.class_ "c-error-page__title" $ H.toHtml title
+        H.p ! A.class_ "c-error-page__desc" $ H.toHtml subtitle
         H.a ! A.class_ "c-button c-button--secondary"
             ! A.href "/" $
           H.span ! A.class_ "c-button__content" $ do
